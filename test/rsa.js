8289a8b8a9a6eb2ea3dde18a1d73c9b88be6b8a5
morakot <morakot@doctoratoz.co>
2019-02-06 09:36:32 +0700

    commit

--- test/rsa.js
+++
@@ -1,6 +0,0 @@
-var generateRSAKeypair = require('generate-rsa-keypair')
- 
-var pair = generateRSAKeypair()
- 
-console.log(pair.private) // String with the private key in PEM format
-console.log(pair.public)
